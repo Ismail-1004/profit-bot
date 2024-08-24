1493c69fd6df7b145ee940c2b9161b5e87a4cb98
Your Name <you@example.com>
2024-08-24 17:47:35 +0500

    main functional added

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,5 @@
 import { Sequelize } from "sequelize";
 
 export default new Sequelize(
-  process.env.DB_NAME, // Название БД
-  process.env.DB_USER, // Имя пользователя
-  process.env.DB_PASSWORD, // Пароль
-  {
-    dialect: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  }
+  process.env.DB_URL
 );
